1c6b099fa9a81419bd9fd31a351856bf09b7d882
beejay <beejay.urzo@gmail.com>
2021-11-10 14:31:32 +0800

    Made it a drop in function

--- df_detect.py
+++ df_detect.py
@@ -1,5 +1,86 @@
 from google.cloud import dialogflow
+from audio_rms import rms, ding
+import os, time
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "google_credentials.json"
+
 SAMPLE_RATE = 16000
+Threshold = 10
+TIMEOUT_LENGTH = 0.5 #The silent length we allow before cutting recognition
+
+def process_audio(ws, session_id, intent_handler):
+    rec = []
+    current = 1
+    end = 0
+
+    while not ws.closed:
+        
+        audio = ws.receive()
+        if isinstance(audio, str):
+            continue #if this is a string, we don't handle it
+        rms_val = rms(audio)
+
+
+        #If audio is loud enough, set the current timeout to now and end timeout to now + TIMEOUT_LENGHT
+        #This will start the next part that stores the audio until it's quiet again
+        if rms_val > Threshold and not current <= end :
+            print("Heard Something")
+            current = time.time()
+            end = time.time() + TIMEOUT_LENGTH
+
+        #If levels are higher than threshold add audio to record array and move the end timeout to now + TIMEOUT_LENGTH
+        #When the levels go lower than threshold, continue recording until timeout. 
+        #By doing this, we only capture relevant audio and not continously call our STT/NLP with nonsensical sounds
+        #By adding a trailing TIMEOUT_LENGTH we can capture natural pauses and make things not sound robotic
+        if current <= end: 
+            if rms_val >= Threshold: end = time.time() + TIMEOUT_LENGTH
+            current = time.time()
+            rec.append(audio)
+
+        #process audio if we have an array of non-silent audio
+        else:
+            if len(rec)>0: 
+                print("Audio Processing")
+
+                output_audio = bytearray(ding()) #get our ding binary
+
+                #When Devs can't name variables
+                i = 0 
+                
+                #send Ding
+                #Chunk out the output audio in 640 bytes and send to socket
+                while (i <= len(output_audio)):
+                    chunk = output_audio[i:i+640]
+                    ws.send(chunk)
+                    i +=640 
+
+                #we join the bytearray into one big binary audio
+                audio_to_process = b''.join(rec)
+                #send it to Dialog flow (or any STT + NLP Combo you want. Using DF here since it'll handle STT and NLP)
+                response = detect_intent_audio("move-test-xpjn", session_id, "en-US", audio_to_process)
+
+                #make DF output audio into a byte array so we can chunk it easily 
+                output_audio = bytearray(response.output_audio) 
+
+                #again, When Devs can't name variables
+                i = 0 
+                
+                #Chunk out the output audio in 640 bytes and send to socket
+                while (i <= len(output_audio)):
+                    chunk = output_audio[i:i+640]
+                    ws.send(chunk)
+                    i +=640 
+                rec = [] #reset audio array to blank
+
+                #Make our code do something about the intent
+                intent_handler(response)
+
+                #If intent is bye, end call
+                if response.query_result.intent.display_name == "bye":
+                    #wait 1 second so the audio can playback completely before cutting off
+                    time.sleep(1)
+                    ws.close()
+
 #copied this from google, modified it a bit so it accepts binary data instead of audio file
 def detect_intent_audio(project_id, session_id, language_code, input_audio):
 
--- server.py
+++ server.py
@@ -1,16 +1,14 @@
 from flask import Flask, request, jsonify
 from flask_sockets import Sockets
-from audio_rms import rms, ding
-from df_detect import detect_intent_audio
+
+from df_detect import detect_intent_audio, process_audio
 import time
 import os
 import uuid
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "google_credentials.json"
 
-Threshold = 10
+
 CHANNELS = 1
-TIMEOUT_LENGTH = 0.5 #The silent length we allow before cutting recognition
 CHUNK_SIZE = 640 #Required Chunk Size by Vonage for 1600
 PACKAGE_TIMEOUT = 4
 package = ''
@@ -82,77 +80,7 @@ def events():
 
 @sockets.route("/socket", methods=["GET"])
 def echo_socket(ws):
-    rec = []
-    current = 1
-    end = 0
-
-    while not ws.closed:
-        
-        audio = ws.receive()
-        if isinstance(audio, str):
-            continue #if this is a string, we don't handle it
-        rms_val = rms(audio)
-
-
-        #If audio is loud enough, set the current timeout to now and end timeout to now + TIMEOUT_LENGHT
-        #This will start the next part that stores the audio until it's quiet again
-        if rms_val > Threshold and not current <= end :
-            print("Heard Something")
-            current = time.time()
-            end = time.time() + TIMEOUT_LENGTH
-
-        #If levels are higher than threshold add audio to record array and move the end timeout to now + TIMEOUT_LENGTH
-        #When the levels go lower than threshold, continue recording until timeout. 
-        #By doing this, we only capture relevant audio and not continously call our STT/NLP with nonsensical sounds
-        #By adding a trailing TIMEOUT_LENGTH we can capture natural pauses and make things not sound robotic
-        if current <= end: 
-            if rms_val >= Threshold: end = time.time() + TIMEOUT_LENGTH
-            current = time.time()
-            rec.append(audio)
-
-        #process audio if we have an array of non-silent audio
-        else:
-            if len(rec)>0: 
-                print("Audio Processing")
-
-                output_audio = bytearray(ding()) #get our ding binary
-
-                #When Devs can't name variables
-                i = 0 
-                
-                #send Ding
-                #Chunk out the output audio in 640 bytes and send to socket
-                while (i <= len(output_audio)):
-                    chunk = output_audio[i:i+640]
-                    ws.send(chunk)
-                    i +=640 
-
-                #we join the bytearray into one big binary audio
-                audio_to_process = b''.join(rec)
-                #send it to Dialog flow (or any STT + NLP Combo you want. Using DF here since it'll handle STT and NLP)
-                response = detect_intent_audio("move-test-xpjn", session_id, "en-US", audio_to_process)
-
-                #make DF output audio into a byte array so we can chunk it easily 
-                output_audio = bytearray(response.output_audio) 
-
-                #again, When Devs can't name variables
-                i = 0 
-                
-                #Chunk out the output audio in 640 bytes and send to socket
-                while (i <= len(output_audio)):
-                    chunk = output_audio[i:i+640]
-                    ws.send(chunk)
-                    i +=640 
-                rec = [] #reset audio array to blank
-
-                #Make our code do something about the intent
-                intent_handler(response)
-
-                #If intent is bye, end call
-                if response.query_result.intent.display_name == "bye":
-                    #wait 1 second so the audio can playback completely before cutting off
-                    time.sleep(1)
-                    ws.close()
+    process_audio(ws, session_id, intent_handler)
 
 
 if __name__ == "__main__":
